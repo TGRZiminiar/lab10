3564cde292c4180be428b35e35c76721d5cdbace
mix <mix@mixs-MacBook-Pro.local>
2023-11-25 22:28:57 +0700

    change timer to binary

--- nwmix/DigitalClock.v
+++ nwmix/DigitalClock.v
@@ -3,12 +3,12 @@ module DigitalClock (
     input clk,
     input sw,
     input trigger,
-    output reg [3:0] s1,
-    output reg [3:0] s2,
-    output reg [3:0] m1,
-    output reg [3:0] m2,
-    output reg [3:0] h1,
-    output reg [3:0] h2,
+    output [3:0] s1,
+    output [3:0] s2,
+    output [3:0] m1,
+    output [3:0] m2,
+    output [3:0] h1,
+    output [3:0] h2,
     output reg pos,
     input btnC, btnU, btnL, btnR, btnD,
     output ledD1, ledD2, ledD3, ledD4
@@ -36,108 +36,70 @@ module DigitalClock (
     localparam numberFive = 4'b0101;
     localparam numberNine = 4'b1001;
     
-    always @(posedge trigger) begin
-        if(sw) begin
-            h1 <= 4'b0000;
-            h2 <= 4'b0000;
-            m1 <= 4'b0000;
-            m2 <= 4'b0000;
-            s1 <= 4'b0000;
-            s2 <= 4'b0000;
+    reg [5:0] hour = 0, min = 0, sec = 0; // max is 60 2^6 = 64
+    integer clkc = 0;
+    localparam onesec = 100_000_00; // 1 second
+  
+     always @(posedge clk) begin
+        if(rst == 1'b1) begin
+            // reset everything to 0
+            {hour, min, sec} <= 0;
         end
-        // Update seconds
-        else if (s1 == numberNine && s2 == numberFive) begin
-            s1 <= 4'b0000;
-            s2 <= 4'b0000;
 
-            // Update minutes
-            if (m1 == numberNine && m2 == numberFive) begin
-                m1 <= 4'b0000;
-                m2 <= 4'b0000;
+        // set clock
+        // minute up btn on
+        else if(minup == 1'b1) begin
+            if(min == 6'd59) begin
+                min <= 0;
+            end
+            else begin
+                min <= min + 1'd1;
+            end
+        end
+        // hour up btn on
+        else if(hrup == 1'b1) begin
+            if(hour == 23) begin
+                hour <= 0;
+            end
+            else begin
+                hour <= hour + 1'd1;
+            end
+        end
 
-                // Update hours
-                if (h1 == numberFour && h2 == numberTwo) begin
-                    h1 <= 4'b0000;
-                    h2 <= 4'b0000;
-                end 
-                else if (h1 == numberNine) begin
-                    h2 <= h2 + 4'b0001;
-                    h1 <= 4'b0000;
-                end 
-                else if (h2 != numberTwo) begin
-                    h1 <= h1 + 4'b0001;
+        // count 
+        else if (en == 1'b1) begin
+            if (clkc == onesec) begin
+                clkc <= 0;
+                if (sec == 6'd59) begin
+                    sec <= 0;
+                    if (min == 6'd59) begin
+                        min <= 0;
+                        if (hour == 6'd23) begin
+                            hour <= 0;
+                        end
+                        else begin
+                            hour <= hour + 1'd1;
+                        end
+                    end
+                    else begin
+                        min <= min + 1'd1;
+                    end
                 end
-
-            end else if (m1 == numberNine) begin
-                m2 <= m2 + 4'b0001;
-                m1 <= 4'b0000;
-            end else begin
-                m1 <= m1 + 4'b0001;
-            end
-        end 
-        else if (s1 == numberNine) begin
-            s2 <= s2 + 4'b0001;
-            s1 <= 4'b0000;
-        end 
-        else begin
-            s1 <= s1 + 4'b0001;
-        end
-
-
-       
-
-    end
-
-    always @(posedge clk) begin
-         // Button Hanlder
-        if(btnL) begin
-            if(pos != 4) begin
-                pos = pos + 1;
+                else begin
+                    sec <= sec + 1'd1;
+                end
             end
             else begin
-                pos = 1;
-            end
-        end
-        else if(btnR) begin
-            if(pos != 1) begin
-                pos = pos - 1;
-            end
-            else begin
-                pos = 4;
-            end
-        end
-
-        else if(btnU) begin
-            if(pos == 1) begin
-                m1 <= m1 + numberOne;
-            end
-            else if(pos == 2) begin
-                m2 <= m2 + numberOne;
-            end
-            else if(pos == 3) begin
-                h1 <= h1 + numberOne;
-            end
-            else if(pos == 4) begin
-                h2 <= h2 + numberOne;
-            end
-        end
-        else if (btnD) begin
-            if(pos == 1) begin
-                m1 <= m1 - numberOne;
-            end
-            else if(pos == 2) begin
-                m2 <= m2 - numberOne;
-            end
-            else if(pos == 3) begin
-                h1 <= h1 - numberOne;
-            end
-            else if(pos == 4) begin
-                h2 <= h2 - numberOne;
+                clkc <= clkc + 1;
             end
         end
     end
 
-   
+    BinaryToBcd second(.binary(sec), .tens(s2), .ones(s1));    
+    BinaryToBcd minute(.binary(min), .tens(m2), .ones(m1));    
+    BinaryToBcd hourBinary(.binary(hour), .tens(h2), .ones(h1));    
+
+
     
 endmodule
 
